a6b4a04037c20330577174ae30b0d16c6f5d6499
ArtNewWave <31260439+ArtNewWave@users.noreply.github.com>
2017-11-26 19:50:49 -0800

    Filter By Type Assignment

---
+++ FilterByType.py
@@ -0,0 +1,348 @@
+#FILTER BY TYPE
+
+
+
+
+sI = 45
+mI = 100
+bI = 455
+eI = 0
+spI = -23
+sS = 'Rubber baby buggy bumpers'
+mS = 'Experience is simply the name we give our mistakes'
+bS = 'Tell me and I forget. Teach me and I remember. Involve me and I learn.'
+eS = ' '
+aL = [1,7,4,21]
+mL = [3,5,7,34,3,2,113,65,8,89]
+lL = [4,34,22,68,9,13,3,5,7,9,2,12,45,923]
+eL = []
+spL = ['name','address','phone number','social security number']
+
+
+value = sI
+print sI 
+
+tester = type(value)
+if tester is int:
+    if value >= 100:
+        print 'That\'s a big number!'
+    else:
+        print 'That\'s a small number!'
+elif tester is str:
+    if len(value) >= 50:
+        print 'Long sentence.'
+    else:
+        print 'Short sentence.'
+elif isinstance(value, list):
+    if len(value) >= 10:
+        print 'Big list!'
+    else: 
+        print 'Short list.' 
+
+################################################
+
+value = mI
+print mI 
+
+tester = type(value)
+if tester is int:
+    if value >= 100:
+        print 'That\'s a big number!'
+    else:
+        print 'That\'s a small number!'
+elif tester is str:
+    if len(value) >= 50:
+        print 'Long sentence.'
+    else:
+        print 'Short sentence.'
+elif isinstance(value, list):
+    if len(value) >= 10:
+        print 'Big list!'
+    else: 
+        print 'Short list.' 
+
+################################################
+
+value = bI
+print bI 
+
+tester = type(value)
+if tester is int:
+    if value >= 100:
+        print 'That\'s a big number!'
+    else:
+        print 'That\'s a small number!'
+elif tester is str:
+    if len(value) >= 50:
+        print 'Long sentence.'
+    else:
+        print 'Short sentence.'
+elif isinstance(value, list):
+    if len(value) >= 10:
+        print 'Big list!'
+    else: 
+        print 'Short list.' 
+
+################################################
+
+value = eI
+print eI 
+
+tester = type(value)
+if tester is int:
+    if value >= 100:
+        print 'That\'s a big number!'
+    else:
+        print 'That\'s a small number!'
+elif tester is str:
+    if len(value) >= 50:
+        print 'Long sentence.'
+    else:
+        print 'Short sentence.'
+elif isinstance(value, list):
+    if len(value) >= 10:
+        print 'Big list!'
+    else: 
+        print 'Short list.' 
+
+################################################
+
+value = spI
+print spI 
+
+tester = type(value)
+if tester is int:
+    if value >= 100:
+        print 'That\'s a big number!'
+    else:
+        print 'That\'s a small number!'
+elif tester is str:
+    if len(value) >= 50:
+        print 'Long sentence.'
+    else:
+        print 'Short sentence.'
+elif isinstance(value, list):
+    if len(value) >= 10:
+        print 'Big list!'
+    else: 
+        print 'Short list.' 
+
+################################################
+
+value = sS
+print sS 
+
+tester = type(value)
+if tester is int:
+    if value >= 100:
+        print 'That\'s a big number!'
+    else:
+        print 'That\'s a small number!'
+elif tester is str:
+    if len(value) >= 50:
+        print 'Long sentence.'
+    else:
+        print 'Short sentence.'
+elif isinstance(value, list):
+    if len(value) >= 10:
+        print 'Big list!'
+    else: 
+        print 'Short list.' 
+
+################################################
+
+value = mS
+print mS 
+
+tester = type(value)
+if tester is int:
+    if value >= 100:
+        print 'That\'s a big number!'
+    else:
+        print 'That\'s a small number!'
+elif tester is str:
+    if len(value) >= 50:
+        print 'Long sentence.'
+    else:
+        print 'Short sentence.'
+elif isinstance(value, list):
+    if len(value) >= 10:
+        print 'Big list!'
+    else: 
+        print 'Short list.' 
+
+################################################
+
+value = mS
+print mS 
+
+tester = type(value)
+if tester is int:
+    if value >= 100:
+        print 'That\'s a big number!'
+    else:
+        print 'That\'s a small number!'
+elif tester is str:
+    if len(value) >= 50:
+        print 'Long sentence.'
+    else:
+        print 'Short sentence.'
+elif isinstance(value, list):
+    if len(value) >= 10:
+        print 'Big list!'
+    else: 
+        print 'Short list.' 
+
+################################################
+
+value = bS
+print bS 
+
+tester = type(value)
+if tester is int:
+    if value >= 100:
+        print 'That\'s a big number!'
+    else:
+        print 'That\'s a small number!'
+elif tester is str:
+    if len(value) >= 50:
+        print 'Long sentence.'
+    else:
+        print 'Short sentence.'
+elif isinstance(value, list):
+    if len(value) >= 10:
+        print 'Big list!'
+    else: 
+        print 'Short list.' 
+
+################################################
+
+value = eS
+print eS 
+
+tester = type(value)
+if tester is int:
+    if value >= 100:
+        print 'That\'s a big number!'
+    else:
+        print 'That\'s a small number!'
+elif tester is str:
+    if len(value) >= 50:
+        print 'Long sentence.'
+    else:
+        print 'Short sentence.'
+elif isinstance(value, list):
+    if len(value) >= 10:
+        print 'Big list!'
+    else: 
+        print 'Short list.' 
+
+################################################
+
+value = aL
+print aL 
+
+tester = type(value)
+if tester is int:
+    if value >= 100:
+        print 'That\'s a big number!'
+    else:
+        print 'That\'s a small number!'
+elif tester is str:
+    if len(value) >= 50:
+        print 'Long sentence.'
+    else:
+        print 'Short sentence.'
+elif isinstance(value, list):
+    if len(value) >= 10:
+        print 'Big list!'
+    else: 
+        print 'Short list.' 
+
+################################################
+
+value = mL
+print mL 
+
+tester = type(value)
+if tester is int:
+    if value >= 100:
+        print 'That\'s a big number!'
+    else:
+        print 'That\'s a small number!'
+elif tester is str:
+    if len(value) >= 50:
+        print 'Long sentence.'
+    else:
+        print 'Short sentence.'
+elif isinstance(value, list):
+    if len(value) >= 10:
+        print 'Big list!'
+    else: 
+        print 'Short list.' 
+
+################################################
+
+value = lL
+print lL 
+
+tester = type(value)
+if tester is int:
+    if value >= 100:
+        print 'That\'s a big number!'
+    else:
+        print 'That\'s a small number!'
+elif tester is str:
+    if len(value) >= 50:
+        print 'Long sentence.'
+    else:
+        print 'Short sentence.'
+elif isinstance(value, list):
+    if len(value) >= 10:
+        print 'Big list!'
+    else: 
+        print 'Short list.' 
+
+################################################
+
+value = eL
+print eL 
+
+tester = type(value)
+if tester is int:
+    if value >= 100:
+        print 'That\'s a big number!'
+    else:
+        print 'That\'s a small number!'
+elif tester is str:
+    if len(value) >= 50:
+        print 'Long sentence.'
+    else:
+        print 'Short sentence.'
+elif isinstance(value, list):
+    if len(value) >= 10:
+        print 'Big list!'
+    else: 
+        print 'Short list.' 
+
+################################################
+
+value = spL
+print spL
+
+tester = type(value)
+if tester is int:
+    if value >= 100:
+        print 'That\'s a big number!'
+    else:
+        print 'That\'s a small number!'
+elif tester is str:
+    if len(value) >= 50:
+        print 'Long sentence.'
+    else:
+        print 'Short sentence.'
+elif isinstance(value, list):
+    if len(value) >= 10:
+        print 'Big list!'
+    else: 
+        print 'Short list.' 
